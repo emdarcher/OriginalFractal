c3a62346bf1f1fb8fa0db0e4e0c0e1192ad32ccb
Ethan Durrant <emd519@yahoo.com>
2015-01-29 15:01:21 -0800

    got it mostly done and looking cool

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
 /* this program creates a fractal that is similar to a snowflake pattern */
 private int smallestBranch =6;
 private int CENTER_X;
@@ -38,5 +54,14 @@ public void myFractal(int x, int y, double len, double angle, int new_start){
         myFractal(sX,sY,(len-i)/2,angle-branchAngle,0);
       }
     } 
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
